3f716ce1639d57e0af7936f64ef1096c2211fe7f
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-06-17 10:38:20 -0300

    Redirecionando para a institution.html via javascript para que seja possível settar as flags dependendo de qual botão o usuário clicou

--- public/js/btnAjuda.js
+++ public/js/btnAjuda.js
@@ -1,33 +1,27 @@
 
 
-let btnFood = document.querySelector("#btnFood");
-let btnMoney = document.querySelector("#btnMoney");
-let btnMedicine = document.querySelector("#btnMedicine");
-let btnClothe = document.querySelector("#btnClothe");
+let btnFood = document.querySelector("#btnFood")
+let btnMoney = document.querySelector("#btnMoney")
+let btnMedicine = document.querySelector("#btnMedicine")
+let btnClothe = document.querySelector("#btnClothe")
 
-/*
+
 btnFood.addEventListener("click", function(event) {
-
-    event.preventDefault();
-    alert("Food");
-});
-
-btnMoney.addEventListener("click", function(event) {
-
-    event.preventDefault();
-    alert("Money");
-});
-
-btnMedicine.addEventListener("click", function(event) {
-
-    event.preventDefault();
-    alert("Medicine");
-});
-
-btnClothe.addEventListener("click", function(event) {
-
-    event.preventDefault();
-    alert("Clothe");
+    redirectingToInstitution()
 })
 
-*/
+btnMoney.addEventListener("click", function(event) {
+    redirectingToInstitution()
+})
+
+btnMedicine.addEventListener("click", function(event) {
+    redirectingToInstitution()
+})
+
+btnClothe.addEventListener("click", function(event) {
+    redirectingToInstitution()
+})
+
+function redirectingToInstitution() {
+    window.location.href = "html/institution.html";
+}
