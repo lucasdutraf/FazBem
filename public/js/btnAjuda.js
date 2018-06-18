e8910c4e51473fa29ee0ea876db3a6980ab26235
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-06-17 23:19:17 -0300

    Testando o envio de dados para o Database do Firebase

--- public/js/btnAjuda.js
+++ public/js/btnAjuda.js
@@ -1,5 +1,4 @@
 
-
 let btnFood = document.querySelector("#btnFood")
 let btnMoney = document.querySelector("#btnMoney")
 let btnMedicine = document.querySelector("#btnMedicine")
@@ -25,6 +24,7 @@ btnClothe.addEventListener("click", function(event) {
     redirectingToInstitution()
     localStorage.setItem("clickedButton", "roupa")
 })
+
 
 function redirectingToInstitution() {
     window.location.href = "html/institution.html";
--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,11 +1,21 @@
-  
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyATU-j8JH1Blwx0wUG2ZKQ0Nln04xgjJBQ",
-    authDomain: "faz-bem.firebaseapp.com",
-    databaseURL: "https://faz-bem.firebaseio.com",
-    projectId: "faz-bem",
-    storageBucket: "faz-bem.appspot.com",
-    messagingSenderId: "253971616099"
-  };
-  firebase.initializeApp(config);
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyATU-j8JH1Blwx0wUG2ZKQ0Nln04xgjJBQ",
+  authDomain: "faz-bem.firebaseapp.com",
+  databaseURL: "https://faz-bem.firebaseio.com",
+  projectId: "faz-bem",
+  storageBucket: "faz-bem.appspot.com",
+  messagingSenderId: "253971616099"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database()
+var ref = database.ref('institutions')
+
+var data = {
+  name: 'teste',
+  adress: 'rua 3'
+}
+
+ref.push(data)
