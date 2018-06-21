1e1f4df9cbfa916f83cf4eb291ba394e90a6c8fd
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-06-20 23:02:22 -0300

    modularizando o código separando as funções de callback necessárias para pergar os nodes do firebase

--- public/js/institutionPage.js
+++ public/js/institutionPage.js
@@ -1,40 +1,13 @@
 
+
  
 let clickedButton = localStorage.getItem("@help-option")
 document.getElementById("titulo-tipo-de-ajuda").innerHTML = "Instituições que precisam de " + clickedButton
-
 
 let database = firebase.database()
 var ref = database.ref('institutions/' + getHelpOption())
 ref.on('value', gotData, errData)
 
-
-    
-function gotData(data) {
-
-    let institutions = data.val();
-    let keys = Object.keys(institutions)
-    var name = []
-    var address = [] 
-    var contact = []
-
-    for(let i = 0; i < keys.length; i++) {
-        let k = keys[i]
-        name[i] = institutions[k].name
-        address[i] = institutions[k].address
-        contact[i] = institutions[k].contact
-    }
-    localStorage.setItem("@institution-names", name);
-    localStorage.setItem("@institution-addresses", address);
-    localStorage.setItem("@institution-contacts", contact);
-
-    document.getElementById("sobre-institu").innerHTML = name
-}
-
-function errData(err) { 
-    alert("Não foi possível carregar as instituições")
-    console.log(err) 
-}
 
 function getHelpOption() {
 
