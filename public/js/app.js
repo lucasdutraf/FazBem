eab7f63344675cbf9e87c9c2985bac2da82049a8
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-06-24 00:47:19 -0300

    Refactoring btnAjuda.js to app.js, the entry point of the application

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,29 @@
-import firebaseInitializer from './firebase'
-import initHelpButtons from './btnAjuda'
+
+let btnFood = document.querySelector("#btnFood")
+let btnMoney = document.querySelector("#btnMoney")
+let btnMedicine = document.querySelector("#btnMedicine")
+let btnClothe = document.querySelector("#btnClothe")
+    
+btnFood.addEventListener("click", function(event) {
+    redirectToInstitutions()
+    localStorage.setItem("@help-option", "comida")
+})
+
+btnMoney.addEventListener("click", function(event) {
+    redirectToInstitutions()
+    localStorage.setItem("@help-option", "dinheiro")
+})
+
+btnMedicine.addEventListener("click", function(event) {
+    redirectToInstitutions()
+    localStorage.setItem("@help-option", "remédio")
+})
+
+btnClothe.addEventListener("click", function(event) {
+    redirectToInstitutions()
+    localStorage.setItem("@help-option", "roupa")
+})
+
+function redirectToInstitutions() {
+    window.location.href = "html/select-institution.html";
+}
