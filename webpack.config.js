8b3ff91aafd08288875564e9f4710cfc9af5bf3a
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-06-24 00:04:23 -0300

    Refactoring all the config for webpack from package.json to webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+
+const path = require('path');
+
+module.exports = {
+    entry: './public/js/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
