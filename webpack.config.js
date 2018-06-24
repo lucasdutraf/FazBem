8887d1ba8e24ce24b924dd9758553279c3cb1502
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-06-24 00:22:39 -0300

    Adding webpack-dev-server  and creating build:prod for running the minified version of the projectJS for production

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
     entry: './public/js/app.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+        path: path.resolve(__dirname, './public/dist'),
+        publicPath: './public/dist'
+    },
 };
